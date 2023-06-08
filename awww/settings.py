2d7ddeacb8e8ffb60ee8054ed9910f6a0e42e60f
Sobczakus1658 <sobczak2002@op.pl>
2023-06-09 01:09:53 +0200

    pobranie

--- awww/settings.py
+++ awww/settings.py
@@ -12,6 +12,16 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import shutil
+
+try:
+    SDCC_PATH = shutil.which('sdcc')
+except:
+    SDCC_PATH = ''
+
+# Ustawienie ścieżki do programu sdcc
+if SDCC_PATH:
+    SDCC_PATH = os.path.abspath(SDCC_PATH)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
