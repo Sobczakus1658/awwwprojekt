4b5de1f4f0fa0dc05e487eb871e18998c0db935c
Sobczakus1658 <sobczak2002@op.pl>
2023-06-08 23:10:32 +0200

    usuniete program.middleware

--- awww/settings.py
+++ awww/settings.py
@@ -53,8 +53,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'program.middleware.SessionDefaultMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'awww.urls'
